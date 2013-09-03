221ca54ecd70d12a2b1a03f7cac4fe503b751e57
Prateek Papriwal <papriwalprateek>
2013-09-04 02:01:09 +0530

    added final code

--- README.md
+++
@@ -1,4 +0,0 @@
-MPTrack
-=======
-
-The source spreadsheet url will be different. Please make changes accordingly.
---
+++ src/Assignment2/visualizations/applets/PoliticsVisApplet.java
@@ -0,0 +1,12 @@
+package Assignment2.visualizations.applets;
+
+import prefuse.util.ui.JPrefuseApplet;
+
+public class PoliticsVisApplet extends JPrefuseApplet {
+	public static String file_name = "/home/ankit/Desktop/CSP301/DataVis/Assignment_2/data/MPTrack-15.xls";
+    public void init() {
+        this.setContentPane(
+            Assignment2.visualizations.PoliticsVis.demo(file_name, "label"));
+    }   
+} 
+
